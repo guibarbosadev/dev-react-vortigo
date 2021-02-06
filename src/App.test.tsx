8b0dd85991a3e08d2840996a64680062222e20c9
Guilherme Barbosa Pereira <guilherme.barbosapereira@outlook.com>
2021-02-06 14:39:36 -0300

    Format code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react'
-import { render, screen } from '@testing-library/react'
-import App from './App'
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
 test('renders react template with snowpack, typescript and tailwind css', () => {
-  expect.hasAssertions()
+    expect.hasAssertions();
 
-  render(<App />)
-  const headingElement = screen.getByRole('heading', {
-    name: /react template with snowpack, typescript and tailwind css/i
-  })
+    render(<App />);
+    const headingElement = screen.getByRole('heading', {
+        name: /react template with snowpack, typescript and tailwind css/i
+    });
 
-  expect(headingElement).toBeInTheDocument()
-})
+    expect(headingElement).toBeInTheDocument();
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import Helmet from 'react-helmet'
+import React from 'react';
+import Helmet from 'react-helmet';
 
 function App(): JSX.Element {
-  return (
-    <div>
-      <Helmet>
-        <title>Template</title>
-      </Helmet>
-      <h1>
-        React template with Snowpack, TypeScript and 
-      </h1>
-    </div>
-  )
+    return (
+        <div>
+            <Helmet>
+                <title>Template</title>
+            </Helmet>
+            <h1>React template with Snowpack, TypeScript and</h1>
+        </div>
+    );
 }
 
-export default App
+export default App;
