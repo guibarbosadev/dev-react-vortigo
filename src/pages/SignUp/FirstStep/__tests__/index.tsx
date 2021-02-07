40dd4d47a8d8550793253d6a634bb6ea55eb193d
Guilherme Barbosa Pereira <guilherme.barbosapereira@outlook.com>
2021-02-07 18:55:50 -0300

    Add SignUp first step

---
+++ src/pages/SignUp/FirstStep/__tests__/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { FirstStep, IProps } from '..';
+import userEvent from '@testing-library/user-event';
+
+describe('<FirstStep /> test case', () => {
+    let props: IProps = {
+        onAdvance: () => {}
+    };
+
+    it(`showns "Let's meet each other first?" text`, () => {
+        render(<FirstStep {...props} />);
+        expect(screen.getByText(`Let's meet each other first?`)).toBeInTheDocument();
+    });
+
+    it(`showns "First wee need to know your name..." label`, () => {
+        render(<FirstStep {...props} />);
+        expect(screen.getByLabelText('First we need to know your name...')).toBeInTheDocument();
+    });
+
+    it('calls props.onAdvance', () => {
+        props.onAdvance = jest.fn();
+        render(<FirstStep {...props} />);
+        const text = 'Some name';
+        const nameInput = screen.getByLabelText('First we need to know your name...');
+        const submitButton = screen.getByRole('button');
+
+        expect(props.onAdvance).not.toHaveBeenCalled();
+        userEvent.type(nameInput, text);
+        userEvent.click(submitButton);
+        expect(props.onAdvance).toHaveBeenCalled();
+    });
+});
--- src/pages/SignUp/FirstStep/index.tsx
+++ src/pages/SignUp/FirstStep/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
 export interface IProps {
-    onAdvance: () => void;
+    onAdvance: (name: string) => void;
 }
 
 export const FirstStep: React.FC<IProps> = ({ onAdvance }) => {
-    return <div>FirstStep</div>;
+    const [name, setName] = React.useState('');
+    const handleSubmit = React.useCallback((event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        onAdvance(name);
+    }, []);
+    const handleChange = React.useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    }, []);
+
+    return (
+        <form className="wrapper" onSubmit={handleSubmit}>
+            <h2>Let's meet each other first?</h2>
+            <label htmlFor="nameInput">
+                First we need to know your name...
+                <input type="text" id="nameInput" value={name} onChange={handleChange} />
+            </label>
+            <button type="submit">Avançar</button>
+        </form>
+    );
 };
